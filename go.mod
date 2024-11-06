86c947fdbd1ac67b0ee79ba04686bf4a504e53ad
Ocskó Gábor <ogabor@gmail.com>
2024-11-06 09:03:49 +0100

    .env

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.21.3
 
 require (
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-sqlite3 v1.14.24 // indirect
 )
--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -22,9 +25,20 @@ func main() {
 	routes.SetupUserRoutes()
 	routes.SetupSampleRoutes()
 
-	// Start the server on port 8080
-	fmt.Println("Server is running on http://localhost:8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	// Load .env file
+	if err := godotenv.Load(); err != nil {
+		log.Printf("Warning: .env file not found")
+	}
+
+	// Get PORT from environment variable, default to 8080 if not set
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	// Start the server using PORT from env
+	fmt.Printf("Server is running on http://localhost:%s\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		fmt.Printf("Error starting server: %v\n", err)
 	}
 }
